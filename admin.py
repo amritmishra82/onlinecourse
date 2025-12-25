1fdcbfe501af7483ef437ff642fab4f4858debfd
Mishra Amrit Lalji <115808810+amritmishra82@users.noreply.github.com>
2025-12-25 19:49:52 +0530

    Create admin.py

---
+++ admin.py
@@ -0,0 +1,34 @@
+from django.contrib import admin
+from .models import (
+    Course,
+    Lesson,
+    Question,
+    Choice,
+    Submission,
+    Enrollment
+)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
+
+class LessonAdmin(admin.ModelAdmin):
+    inlines = [QuestionInline]
+
+
+admin.site.register(Course)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
+admin.site.register(Enrollment)
